d6764905183344e02907c52e1578e03bd217f7b8
Codername47 <Codername47@yandex.ru>
2022-07-11 18:42:53 +0300

    initAPI

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthController extends AbstractController
+{
+    #[Route('/login', name: 'app_auth_page')]
+    public function index(): Response
+    {
+        return $this->render('auth/index.html.twig', [
+            'controller_name' => 'AuthController',
+        ]);
+    }
+    #[Route('/auth', name: 'app_auth')]
+    public function auth(): Response
+    {
+        return $this->redirectToRoute('app_auth_page');
+    }
+}
